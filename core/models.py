bda71eb7160062bf989c7197398f0122825f9419
Bleu Serge Alain <reelsergealain@gmail.com>
2023-11-08 18:29:30 +0000

    Creation du model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    brith_date = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    FILIERE_CHOICES = (
+        ('IDE', 'Infirmier Diplômé d\'Etat'),
+        ('SAGEF', 'Sage-Femme'),
+        ('AS', 'Aide-Soignant'),
+        ('AP', 'Auxiliaire'),
+    )
+    filiere = models.CharField(max_length=5, choices=FILIERE_CHOICES)
+    bource = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.last_name + ' ' + self.first_name
+
+
+class Matter(models.Model):
+    name = models.CharField(max_length=255)
+    coefficient = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.coefficient)
+
+class Note(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    matter = models.ForeignKey(Matter, on_delete=models.CASCADE)
+    note = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.student) + ' ' + str(self.matter) + ' ' + str(self.note)
+
+class SchoolYear(models.Model):
+    name = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)

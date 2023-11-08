bf7bd0712c4e5c76508e08c1bc7e1cbdb86634a2
Bleu Serge Alain <reelsergealain@gmail.com>
2023-11-08 18:36:07 +0000

    Creation du model V2

--- core/models.py
+++ core/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 
+class Filiere(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.name)
+    
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     brith_date = models.DateField()
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    FILIERE_CHOICES = (
-        ('IDE', 'Infirmier Diplômé d\'Etat'),
-        ('SAGEF', 'Sage-Femme'),
-        ('AS', 'Aide-Soignant'),
-        ('AP', 'Auxiliaire'),
-    )
-    filiere = models.CharField(max_length=5, choices=FILIERE_CHOICES)
+    filiere = models.ForeignKey(Filiere, on_delete=models.CASCADE)
     bource = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -31,6 +33,7 @@ class Matter(models.Model):
     def __str__(self):
         return str(self.name) + ' ' + str(self.coefficient)
 
+
 class Note(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     matter = models.ForeignKey(Matter, on_delete=models.CASCADE)
@@ -40,6 +43,7 @@ class Note(models.Model):
 
     def __str__(self):
         return str(self.student) + ' ' + str(self.matter) + ' ' + str(self.note)
+
 
 class SchoolYear(models.Model):
     name = models.CharField(max_length=255)
